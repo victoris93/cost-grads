08b86771519cbee9007bb667c0706979cc34d0c1
victoris93 <shevchenko682@gmail.com>
2023-09-25 11:25:39 +0000

    get the vertices of the transmodal peak, 10% grabd thresh

---
+++ get_transmodal_temporal_peak.py
@@ -0,0 +1,24 @@
+import nibabel as nib
+import numpy as np
+
+grads = np.load('/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/COST_group_gradients.npy').T
+grad2L = grads[1][:32492]
+grad2L[np.where(grad2L > np.percentile(grad2L, 10))[0]] = np.nan
+grad2R = grads[1][32492:]
+grad2R[np.where(grad2R > np.percentile(grad2R, 10))[0]] = np.nan
+TpeakTempL = grad2L.copy()
+TpeakTempR = grad2R.copy()
+
+fsLR_labels_L = nib.load('/home/victoria/server/public/scratchpad/surfaces/fsLR/labels/fsLR.32k.L.label.gii').darrays[0].data
+fsLR_labels_R = nib.load('/home/victoria/server/public/scratchpad/surfaces/fsLR/labels/fsLR.32k.R.label.gii').darrays[0].data
+
+temp_zone_indL = np.where(fsLR_labels_L != 5)[0]
+temp_zone_indR = np.where(fsLR_labels_R != 5)[0]
+TpeakTempL[temp_zone_indL] = np.nan
+TpeakTempL = np.where(np.isnan(TpeakTempL) == False)[0]
+TpeakTempR[temp_zone_indR] = np.nan
+TpeakTempR = np.where(np.isnan(TpeakTempR) == False)[0]
+
+np.save('/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/COST_grad2_thresh10_L.npy', TpeakTempL)
+np.save('/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/COST_grad2_thresh10_R.npy', TpeakTempR)
+
