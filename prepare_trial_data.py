af9cc024c0fd72137d1862d82b7da99bd8231469
victoris93 <shevchenko682@gmail.com>
2023-10-27 16:27:15 +0200

    rm duplicates

--- prepare_trial_data.py
+++ prepare_trial_data.py
@@ -4,15 +4,16 @@ import pandas as pd
 data_path = '/home/victoria/server/data/COST/COST_mri/derivatives/rest'
 task_data_path = './task_data'
 
-compile_trial_data(data_path, task_data_path, save_to = './task_data/all_data_raw.csv')
+# compile_trial_data(data_path, task_data_path, save_to = './task_data/all_data_raw.csv')
 
-task_data = pd.read_csv('./task_data/all_data_raw.csv')
-task_data = task_data.rename(columns={'Subject': 'participant_id'})
-task_data.to_csv('./task_data/all_data_raw.csv', index=False)
+# task_data = pd.read_csv('./task_data/all_data_raw.csv')
+# task_data = task_data.rename(columns={'Subject': 'participant_id'})
+# task_data.to_csv('./task_data/all_data_raw.csv', index=False)
 
 data = prepare_distance_task_data('./task_data/all_data_raw.csv', 'distances.csv', False)
 dem_vars = pd.read_csv('./SIFI_full.csv')[["participant_id", "sex", "age"]]
 data = data.merge(dem_vars, on='participant_id')
+data = data.drop_duplicates()
 
 data['FlashType'] = data['Condition'].map({1:1, 2:1, 3:1, 4:2, 5:2, 6:2, 7:3, 8:3, 9:3, 10:4, 11:4, 12:4})
 data['NrBeeps'] = data['Condition'].map({1:0, 4:0, 7:0, 10:0, 2:1, 5:1, 8:1, 11:1, 3:2, 6:2, 9:2, 12:2})
