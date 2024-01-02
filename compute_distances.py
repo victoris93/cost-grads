ed8243133a7164ac581bf058ecd773cc0d86ecd9
victoris93 <shevchenko682@gmail.com>
2024-01-02 15:20:54 +0100

    distances to peaks, aparc links fixed

--- compute_distances.py
+++ compute_distances.py
@@ -7,9 +7,23 @@ sys.path.append('/home/victoria/surfdist')
 import pandas as pd
 from NeuroConn.preprocessing.preprocessing import FmriPreppedDataSet
 from surfdist import analysis
-from utils import get_transmodal_node
 import tqdm
 import csv
+
+level = sys.argv[1]
+zone = sys.argv[2]
+
+if level == 'ind':
+    level = True
+    ind= 'individual'
+
+else:
+    level = False
+    ind = 'group'
+
+if not level:
+    TpeakL = np.load('/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/COST_grad2_thresh5_L.npy')
+    TpeakR = np.load('/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/COST_grad2_thresh5_R.npy')
 
 data_path = '/home/victoria/server/data/COST/COST_mri/derivatives/rest'
 subjects = FmriPreppedDataSet(data_path).subjects.tolist()
@@ -21,6 +35,9 @@ cortex_R = np.where(fsLR_labels_R != 0)[0]
 
 for subject in tqdm.tqdm(subjects):
     try:
+        if level:
+            TpeakL = np.load(f'/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/thresholded/aligned_grad2_thresh5_L_{zone}_sub-{subject}.npy')
+            TpeakR = np.load(f'/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/gradients/thresholded/aligned_grad2_thresh5_R_{zone}_sub-{subject}.npy')
 
         surfL = nib.load(f"/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/sub-{subject}/anat/sub-{subject}_hemi-L_midthickness.32k_fs_LR.surf.gii")
         surfR = nib.load(f"/home/victoria/server/data/COST/COST_mri/derivatives/rest/derivatives/sub-{subject}/anat/sub-{subject}_hemi-R_midthickness.32k_fs_LR.surf.gii")
@@ -41,23 +58,25 @@ for subject in tqdm.tqdm(subjects):
         v1L = np.where((parc_sub_L == 43) | (parc_sub_L == 45))[0]
         v1R = np.where((parc_sub_R == 43) | (parc_sub_R == 45))[0]
 
-        dist_a1_v1_L = analysis.calc_roi_dist(surfL, cortex_L, a1L, v1L, dist_type='min')
-        dist_a1_v1_R = analysis.calc_roi_dist(surfR, cortex_R, a1L, v1R, dist_type='min')
+        dist_Tpeak_a1_L = analysis.calc_roi_dist(surfL, cortex_L, TpeakL, a1L, dist_type='min')
+        dist_Tpeak_a1_R = analysis.calc_roi_dist(surfR, cortex_R, TpeakR, a1R, dist_type='min')
 
-        if not os.path.exists(f'./distances_a1_v1.csv'):
-            df = pd.DataFrame(columns=['participant_id', 'Dist A1 to V1, LH', 'Dist A1 to V1, RH'])
-            df.to_csv('./distances_a1_v1.csv', index=False)
+        dist_Tpeak_v1_L = analysis.calc_roi_dist(surfL, cortex_L, TpeakL, v1L, dist_type='min')
+        dist_Tpeak_v1_R = analysis.calc_roi_dist(surfR, cortex_R, TpeakR, v1R, dist_type='min')
+
+        if not os.path.exists(f'./distances_{ind}_Tpeak.csv'):
+            df = pd.DataFrame(columns=['participant_id', 'Dist to A1, LH', 'Dist to A1, RH', 'Dist to V1, LH', 'Dist to V1, RH'])
+            df.to_csv('./distances_{ind}_Tpeak.csv', index=False)
 
         print(f"Writing results for subject {subject}")
-        with open('distances_a1_v1.csv', mode='a', newline='') as file:
+        with open(f'distances_{ind}_Tpeak.csv', mode='a', newline='') as file:
             # Create a CSV writer object
             writer = csv.writer(file)
 
             # Write a new row to the CSV file
-            writer.writerow([subject, dist_a1_v1_L, dist_a1_v1_R])
+            writer.writerow([subject, dist_Tpeak_a1_L, dist_Tpeak_a1_R, dist_Tpeak_v1_L, dist_Tpeak_v1_R])
     except Exception as e:
         print(f"Error for subject {subject}: {e}")
 
 print("Distances computed.")
-
 
